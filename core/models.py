b6846f7c7e8a95fe07bc42cf5ae07d2be8400bd3
hossein <shsamiei27ap@gmail.com>
2022-12-15 17:01:22 +0330

    feat: student model added to project

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    student_id = models.CharField(max_length=12, unique=True, db_index=True)
+    phone_number = models.CharField(max_length=20)
+    enter_year = models.IntegerField()
+
