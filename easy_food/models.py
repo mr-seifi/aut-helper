157bdaa328153b20731e65be783e1a3afe0589df
hossein <shsamiei27ap@gmail.com>
2022-12-15 17:18:40 +0330

    feat: easy_food model added

--- easy_food/models.py
+++ easy_food/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from core.models import Student
+from .enums import FoodChoices
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(choices=FoodChoices)
+    student = models.ForeignKey(to=Student)
+
