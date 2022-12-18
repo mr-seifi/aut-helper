8c1d6e9cd9eabe5570c29b0e355d36d7ccc9e289
super amin <aminseiifi@gmail.com>
2022-12-18 16:28:33 +0330

    feat(book): add db fields

--- easy_book/models.py
+++ easy_book/models.py
@@ -4,6 +4,7 @@ from core.models import Student
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    id = models.IntegerField()
-    student = models.ManyToManyField(to=Student)
-
+    isbn = models.CharField(max_length=128, unique=True, db_index=True)
+    row = models.CharField(max_length=8)
+    col = models.CharField(max_length=8)
+    created = models.DateTimeField(auto_now_add=True)
