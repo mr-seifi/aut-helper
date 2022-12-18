5dfc279d4256335bc16c3fc9c6f056633dd7bfb4
super amin <aminseiifi@gmail.com>
2022-12-18 16:29:41 +0330

    fix(food): fix food model fields

--- easy_food/models.py
+++ easy_food/models.py
@@ -4,7 +4,7 @@ from .enums import FoodChoices
 
 
 class Food(models.Model):
-    name = models.CharField(choices=FoodChoices)
+    name = models.CharField(max_length=128, choices=FoodChoices.choices)
     student = models.ForeignKey(to=Student, on_delete=models.CASCADE)
     reserved_date = models.DateField()
     created = models.DateTimeField(auto_now_add=True)
