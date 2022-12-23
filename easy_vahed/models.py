8ad319d0fa4994c8b688cf4e66daab8b0edfb61d
Parsa Poorsistani <poorsistani13@gmail.com>
2022-12-23 20:50:44 +0330

    course model modified

--- easy_vahed/models.py
+++ easy_vahed/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from ..core.models import Student
 
 class Course(models.Model):
-    course_name = models.TextField()
-    course_unit = models.IntegerField(max_length=1)
+    name = models.CharField()
+    unit = models.IntegerField(max_length=1)
+    # not sure for class dates 
     class_date1 = models.DateTimeField()
     class_date2 = models.DateTimeField()
     exam_date = models.DateTimeField()
-    student = models.ForeignKey(Student)
+    students = models.ManyToManyField(Student)
+    
+    def __str__(self) -> str:
+        return self.name
     
