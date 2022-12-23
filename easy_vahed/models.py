693b362521c411f1889ca735105e08dc83828229
Parsa Poorsistani <poorsistani13@gmail.com>
2022-12-23 17:03:58 +0330

    course model added

--- easy_vahed/models.py
+++ easy_vahed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ..core.models import Student
 
-# Create your models here.
+class Course(models.Model):
+    course_name = models.TextField()
+    course_unit = models.IntegerField(max_length=1)
+    class_date1 = models.DateTimeField()
+    class_date2 = models.DateTimeField()
+    exam_date = models.DateTimeField()
+    student = models.ForeignKey(Student)
+    
