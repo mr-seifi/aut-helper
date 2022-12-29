08422f1ed7843ab3dbf44d405f50c6ee3638c20d
Parsa Poorsistani <poorsistani13@gmail.com>
2022-12-29 15:02:59 +0330

    __str__ added to student model and admin

--- core/models.py
+++ core/models.py
@@ -8,3 +8,6 @@ class Student(models.Model):
     enter_year = models.IntegerField()
     balance = models.IntegerField(default=0)
     gpa = models.FloatField(default=0)
+    
+    def __str__(self) -> str:
+        return self.student_id
