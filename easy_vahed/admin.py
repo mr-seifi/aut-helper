6933076b2a5e965ca07e435ecbe58a9136201b9d
Parsa Poorsistani <poorsistani13@gmail.com>
2022-12-29 19:49:09 +0330

    day mapping added and course model changed

--- easy_vahed/admin.py
+++ easy_vahed/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Course,Time,Day
+from .models import Course,Day
 
 admin.site.register(Course)
-admin.site.register(Time)
 admin.site.register(Day)
--- easy_vahed/models.py
+++ easy_vahed/models.py
@@ -1,29 +1,28 @@
 from django.db import models
 from core.models import Student
+from day_mapping import persian_day_mapping
 
 class Day(models.Model):
-    day = models.CharField(max_length=20)
+    day = models.IntegerField()
+    
+    def map_day(self):
+        return persian_day_mapping[self.day]
     
     def __str__(self) -> str:
-        return self.day
+        return self.map_day()
+    
     
 class Course(models.Model):
     name = models.CharField(max_length=100)
     unit = models.IntegerField()
     lecturer = models.CharField(max_length=100)
-    # needs improvment
-    students = models.ManyToManyField(Student,blank=True)
+    days = models.ManyToManyField(Day)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    # students = models.ManyToManyField(Student,blank=True)
     
     def __str__(self) -> str:
-        return f'{self.name} - {self.lecturer}'
+        return f'{self.name} - {self.lecturer} - {self.days} - {self.start_time} - {self.end_time}'
     
-class Time(models.Model):
-    day = models.ManyToManyField(Day,blank=True)
-    start_time = models.FloatField()
-    end_time = models.FloatField()
-    course = models.ForeignKey(Course,on_delete=models.CASCADE)
-    
-    def __str__(self) -> str:
-        return f'{self.day} -> {self.start_time} - {self.end_time}'
     
     
