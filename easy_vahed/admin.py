7d53dc5735eaa1a6a2b16121375aa87c674f0080
Parsa Poorsistani <poorsistani13@gmail.com>
2022-12-29 16:02:33 +0330

    time classes renamed

--- easy_vahed/admin.py
+++ easy_vahed/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from models import Course
+from .models import Course,Time,ExamTime
 
 admin.site.register(Course)
+admin.site.register(Time)
+admin.site.register(ExamTime)
--- easy_vahed/models.py
+++ easy_vahed/models.py
@@ -1,15 +1,21 @@
 from django.db import models
-from ..core.models import Student
+from core.models import Student
 
 class Course(models.Model):
-    name = models.CharField()
-    unit = models.IntegerField(max_length=1)
-    # not sure for class dates 
-    class_date1 = models.DateTimeField()
-    class_date2 = models.DateTimeField()
-    exam_date = models.DateTimeField()
-    students = models.ManyToManyField(Student)
+    name = models.CharField(max_length=100)
+    unit = models.IntegerField()
+    lecturer = models.CharField(max_length=100)
+    # needs improvment
+    students = models.ManyToManyField(Student,blank=True)
     
     def __str__(self) -> str:
-        return self.name
+        return f'{self.name} - {self.lecturer}'
     
+class ClassTime(models.Model):
+    day = models.CharField(max_length=100)
+    start_time = models.FloatField()
+    end_time = models.FloatField()
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+
+class ExamTime(ClassTime):
+    date = models.DateField()
