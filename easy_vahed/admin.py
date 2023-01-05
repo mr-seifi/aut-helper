4b378a0d47da08e681403467a16fea29c112d5e0
super amin <aminseiifi@gmail.com>
2023-01-05 15:27:43 +0330

    Revert "nothing for test"
    This reverts commit 5c29d5b90471a0b367604de503f6cecc6f5d35b7.

--- easy_vahed/admin.py
+++ easy_vahed/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Course,Time,Day
+from .models import Course,Time,ExamTime
 
 admin.site.register(Course)
 admin.site.register(Time)
-admin.site.register(Day)
+admin.site.register(ExamTime)
--- easy_vahed/models.py
+++ easy_vahed/models.py
@@ -4,9 +4,6 @@ from core.models import Student
 class Day(models.Model):
     day = models.CharField(max_length=20)
     
-    def __str__(self) -> str:
-        return self.day
-    
 class Course(models.Model):
     name = models.CharField(max_length=100)
     unit = models.IntegerField()
@@ -17,13 +14,13 @@ class Course(models.Model):
     def __str__(self) -> str:
         return f'{self.name} - {self.lecturer}'
     
-class Time(models.Model):
-    day = models.ManyToManyField(Day,blank=True)
+class ClassTime(models.Model):
+    day = models.ManyToManyField(Day,blank=False)
     start_time = models.FloatField()
     end_time = models.FloatField()
     course = models.ForeignKey(Course,on_delete=models.CASCADE)
-    
-    def __str__(self) -> str:
-        return f'{self.day} -> {self.start_time} - {self.end_time}'
+
+class ExamTime(ClassTime):
+    date = models.DateField()
     
     
