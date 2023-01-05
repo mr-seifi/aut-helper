085b7dc3aa2f690c49615a50e60401eaf9cdb056
super amin <aminseiifi@gmail.com>
2023-01-05 15:06:50 +0330

    Revert "feat: api added for Easy Food"
    This reverts commit 96461e838fbd49bd1804258d66b023a634b55fc3.

--- easy_food/views.py
+++ easy_food/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from rest_framework.viewsets import  ModelViewSet
-from easy_food.models import Food
-from easy_food.serializer import EasyFoodSerializer
 
-
-class ShopAdminViewSet(ModelViewSet):
-    serializer_class = EasyFoodSerializer
-    queryset = Food.objects.all()
-
+# Create your views here.
