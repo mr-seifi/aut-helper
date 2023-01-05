35dd4f83fb178a70d2ea042f618e8a1b7fd6ab95
super amin <aminseiifi@gmail.com>
2023-01-05 17:44:32 +0330

    fix(thumb): fix cover url

--- bot.py
+++ bot.py
@@ -423,16 +423,13 @@ async def _search_result(query):
             id=str(uuid4()),
             title=book.title,
             input_message_content=InputTextMessageContent(f'/lookup {book.uid}'),
-            thumb_url=f'http://{os.getenv("DOMAIN")}/cover/{book.cover.url.split("?")[0].split("/")[-1]}'
+            thumb_url=f'http://{os.getenv("DOMAIN")}/cover/{book.id}.'
+                      f'{book.cover.url.split("?")[0].split("/")[-1].split(".")[-1]}'
             if book.cover else '',
             description=f'{book.year + "-" if book.year else ""}'
                         f'{book.authors}\n{book.publisher}'
         ) for book in book_service.search_book(query)
     ]
-
-    for book in book_service.search_book(query):
-        logger.info(f'http://{os.getenv("DOMAIN")}/cover/{book.cover.url.split("?")[0].split("/")[-1]}'
-                    if book.cover else '')
 
     return results
 
