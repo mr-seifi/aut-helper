9f239e0a6c3ea8536fdb3406b752739a99f7cb58
super amin <aminseiifi@gmail.com>
2023-01-05 15:30:04 +0330

    Revert "course model modified"
    This reverts commit 8ad319d0fa4994c8b688cf4e66daab8b0edfb61d.

--- easy_vahed/models.py
+++ easy_vahed/models.py
@@ -2,14 +2,10 @@ from django.db import models
 from ..core.models import Student
 
 class Course(models.Model):
-    name = models.CharField()
-    unit = models.IntegerField(max_length=1)
-    # not sure for class dates 
+    course_name = models.TextField()
+    course_unit = models.IntegerField(max_length=1)
     class_date1 = models.DateTimeField()
     class_date2 = models.DateTimeField()
     exam_date = models.DateTimeField()
-    students = models.ManyToManyField(Student)
-    
-    def __str__(self) -> str:
-        return self.name
+    student = models.ForeignKey(Student)
     
